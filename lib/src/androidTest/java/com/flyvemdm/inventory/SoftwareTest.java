10dea398cfd2285e6a5d258c6c9ed123b89870bf
Rafael Hernandez <rhernandez@teclib.com>
2017-06-22 08:46:11 -0400

    move Software folder

--- lib/src/androidTest/java/com/flyvemdm/inventory/SoftwareTest.java
+++ lib/src/androidTest/java/com/flyvemdm/inventory/SoftwareTest.java
@@ -1,10 +1,12 @@
-package com.flyvemdm.inventory.categories;
+package com.flyvemdm.inventory;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.flyvemdm.inventory.categories.Software;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
