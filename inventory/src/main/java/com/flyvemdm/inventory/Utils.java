cd758709b78858de95b00ea6c63d967347463736
Rafa Hernández <rhernandez@teclib.com>
2017-07-27 19:11:52 -0400

    fix(test): improve function to detect if is running on emulator

--- inventory/src/main/java/com/flyvemdm/inventory/Utils.java
+++ inventory/src/main/java/com/flyvemdm/inventory/Utils.java
@@ -41,19 +41,85 @@ import java.util.Date;
  */
 public class Utils {
 
+    private static int rating = -1;
+
     private Utils() {
 
     }
 
     public static boolean isEmulator() {
-        return Build.FINGERPRINT.startsWith("generic")
-                || Build.FINGERPRINT.startsWith("unknown")
-                || Build.MODEL.contains("google_sdk")
-                || Build.MODEL.contains("Emulator")
-                || Build.MODEL.contains("Android SDK built for x86")
-                || Build.MANUFACTURER.contains("Genymotion")
-                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-                || "google_sdk".equals(Build.PRODUCT);
+        int newRating = 0;
+        if(rating < 0) {
+            if (Build.PRODUCT.contains("sdk") ||
+                    Build.PRODUCT.contains("Andy") ||
+                    Build.PRODUCT.contains("ttVM_Hdragon") ||
+                    Build.PRODUCT.contains("google_sdk") ||
+                    Build.PRODUCT.contains("Droid4X") ||
+                    Build.PRODUCT.contains("nox") ||
+                    Build.PRODUCT.contains("sdk_x86") ||
+                    Build.PRODUCT.contains("sdk_google") ||
+                    Build.PRODUCT.contains("vbox86p")) {
+                newRating++;
+            }
+
+            if (Build.MANUFACTURER.equals("unknown") ||
+                    Build.MANUFACTURER.equals("Genymotion") ||
+                    Build.MANUFACTURER.contains("Andy") ||
+                    Build.MANUFACTURER.contains("MIT") ||
+                    Build.MANUFACTURER.contains("nox") ||
+                    Build.MANUFACTURER.contains("TiantianVM")){
+                newRating++;
+            }
+
+            if (Build.BRAND.equals("generic") ||
+                    Build.BRAND.equals("generic_x86") ||
+                    Build.BRAND.equals("TTVM") ||
+                    Build.BRAND.contains("Andy")) {
+                newRating++;
+            }
+
+            if (Build.DEVICE.contains("generic") ||
+                    Build.DEVICE.contains("generic_x86") ||
+                    Build.DEVICE.contains("Andy") ||
+                    Build.DEVICE.contains("ttVM_Hdragon") ||
+                    Build.DEVICE.contains("Droid4X") ||
+                    Build.DEVICE.contains("nox") ||
+                    Build.DEVICE.contains("generic_x86_64") ||
+                    Build.DEVICE.contains("vbox86p")) {
+                newRating++;
+            }
+
+            if (Build.MODEL.equals("sdk") ||
+                    Build.MODEL.equals("google_sdk") ||
+                    Build.MODEL.contains("Droid4X") ||
+                    Build.MODEL.contains("TiantianVM") ||
+                    Build.MODEL.contains("Andy") ||
+                    Build.MODEL.equals("Android SDK built for x86_64") ||
+                    Build.MODEL.equals("Android SDK built for x86")) {
+                newRating++;
+            }
+
+            if (Build.HARDWARE.equals("goldfish") ||
+                    Build.HARDWARE.equals("vbox86") ||
+                    Build.HARDWARE.contains("nox") ||
+                    Build.HARDWARE.contains("ttVM_x86")) {
+                newRating++;
+            }
+
+            if (Build.FINGERPRINT.contains("generic/sdk/generic") ||
+                    Build.FINGERPRINT.contains("generic_x86/sdk_x86/generic_x86") ||
+                    Build.FINGERPRINT.contains("Andy") ||
+                    Build.FINGERPRINT.contains("ttVM_Hdragon") ||
+                    Build.FINGERPRINT.contains("generic_x86_64") ||
+                    Build.FINGERPRINT.contains("generic/google_sdk/generic") ||
+                    Build.FINGERPRINT.contains("vbox86p") ||
+                    Build.FINGERPRINT.contains("generic/vbox86p/vbox86p")) {
+                newRating++;
+            }
+
+            rating = newRating;
+        }
+        return rating > 3;
     }
 
     /**
