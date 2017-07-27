d36503bb02d775231b6f13bc66ad9a6327158da2
Rafa Hernández <rhernandez@teclib.com>
2017-07-27 17:14:17 -0400

    fix(travis): remove travis-build.sh file

--- travis-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    gradle clean gnagCheck
-else
-    gradle clean gnagReport -PauthToken="${GNAG_AUTH_TOKEN}" -PissueNumber="${TRAVIS_PULL_REQUEST}" --stacktrace
-fi
