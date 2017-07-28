dd457a5b49156764f3a04b4da25c88467d40a386
Rafa Hernández <rhernandez@teclib.com>
2017-07-28 11:21:52 -0400

    ci(travis): add script to test, build and upload to bintray

---
+++ ci/deploy.sh
@@ -0,0 +1,24 @@
+echo $TRAVIS_BRANCH
+
+# always run unit test
+gradle connectedAndroidTest
+
+if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
+then
+    #create file with credential to upload bintray
+    echo "bintray.user="$BT_USER > "local.properties"
+    echo "bintray.apikey="$BT_USER > "local.properties"
+
+    # send to bintray
+    gradle install
+    gradle bintrayUpload
+fi
+
+if [[ "$TRAVIS_BRANCH" == "feature/package" ]];
+then
+    echo "bintray.user="$BT_USER > "local.properties"
+    echo "bintray.apikey="$BT_USER > "local.properties"
+
+    gradle install
+    gradle bintrayUpload
+fi
