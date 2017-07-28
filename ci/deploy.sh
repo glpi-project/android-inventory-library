e91b9cb363a0028167a7fcdc38916dbbb8076660
Rafa Hernández <rhernandez@teclib.com>
2017-07-28 15:08:12 -0400

    ci(travis): fix deploy on travis branch develop and PR false

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,12 +7,6 @@ echo $TRAVIS_BRANCH
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
     # send to bintray
-    gradle install
-    gradle bintrayUpload
-fi
-
-if [[ "$TRAVIS_BRANCH" == "feature/package" ]];
-then
     ./gradlew install
     ./gradlew bintrayUpload
 fi
