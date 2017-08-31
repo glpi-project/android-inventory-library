1e7718179be4041323ccf41bdff89053d54cb341
Naylin <nmedina@teclib.com>
2017-08-31 18:03:18 -0400

    docs(equals): add param and return tags

--- inventory/src/main/java/com/flyvemdm/inventory/categories/Bluetooth.java
+++ inventory/src/main/java/com/flyvemdm/inventory/categories/Bluetooth.java
@@ -52,6 +52,11 @@ public class Bluetooth extends Categories {
     private static final long serialVersionUID = 3252750764653173048L;
     private BluetoothAdapter adapter;
 
+    /**
+     * Indicates whether some other object is "equal to" this one
+     * @param Object obj the reference object with which to compare
+     * @return boolean true if the object is the same as the one given in argument
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
