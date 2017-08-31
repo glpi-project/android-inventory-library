0737d2daf5393710700a5095e5ec4cc2a3cc03dd
Naylin <nmedina@teclib.com>
2017-08-31 18:09:05 -0400

    docs(hashCode): add the return of the tag

--- inventory/src/main/java/com/flyvemdm/inventory/categories/Inputs.java
+++ inventory/src/main/java/com/flyvemdm/inventory/categories/Inputs.java
@@ -68,6 +68,10 @@ public class Inputs extends Categories {
 		return (!super.equals(obj));
 	}
 
+	/**
+     * Returns a hash code value for the object
+     * @return int a hash code value for the object
+     */
 	@Override
 	public int hashCode() {
 		int hash = super.hashCode();
