a950279804654b1ba6d1f1f173490c99b5497970
Naylin <nmedina@teclib.com>
2017-08-31 18:07:10 -0400

    docs(equals): add param and return tags

--- inventory/src/main/java/com/flyvemdm/inventory/categories/Hardware.java
+++ inventory/src/main/java/com/flyvemdm/inventory/categories/Hardware.java
@@ -68,6 +68,11 @@ public class Hardware extends Categories {
     private Context xCtx;
     private static final String OSNAME = "Android";
 
+    /**
+     * Indicates whether some other object is "equal to" this one
+     * @param Object obj the reference object with which to compare
+     * @return boolean true if the object is the same as the one given in argument
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
