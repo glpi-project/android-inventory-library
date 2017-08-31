15fcabb1bea9b75fb11800feb4dcbd764754dbee
Naylin <nmedina@teclib.com>
2017-08-31 18:05:45 -0400

    docs(equals): add param and return tags

--- inventory/src/main/java/com/flyvemdm/inventory/categories/Categories.java
+++ inventory/src/main/java/com/flyvemdm/inventory/categories/Categories.java
@@ -56,6 +56,11 @@ public class Categories extends ArrayList<Category>{
     private static final long serialVersionUID = 2278660715848751766L;
     private Context mCtx;
 
+    /**
+     * Indicates whether some other object is "equal to" this one
+     * @param Object obj the reference object with which to compare
+     * @return boolean true if the object is the same as the one given in argument
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
