b6d4f62de7a503786d454673822e7b591505a9a8
Naylin <nmedina@teclib.com>
2017-08-31 18:11:39 -0400

    docs(equals): add param and return tags

--- inventory/src/main/java/com/flyvemdm/inventory/categories/Simcards.java
+++ inventory/src/main/java/com/flyvemdm/inventory/categories/Simcards.java
@@ -53,6 +53,11 @@ public class Simcards extends Categories {
 
     private TelephonyManager mTM;
 
+    /**
+     * Indicates whether some other object is "equal to" this one
+     * @param Object obj the reference object with which to compare
+     * @return boolean true if the object is the same as the one given in argument
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
