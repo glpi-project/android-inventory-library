fdb46bab3e2ba623e32509d621c3c6fe727cd1ff
Rafa Hernandez <rhernandez@teclib.com>
2017-09-19 12:30:45 -0400

    ci(circleci): add circleci scripts

---
+++ ci/scripts/ci_setup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+#  Copyright (C) 2017 Teclib'
+#
+#  This file is part of Flyve MDM Inventory Android.
+#
+#  Flyve MDM Inventory Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM Android is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM Inventory Android is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  ------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - rafaelje
+#  @copyright Copyright (c) 2017 Flyve MDM
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/flyve-mdm-android-inventory
+#  @link      http://www.glpi-project.org/
+#  @link      https://flyve-mdm.com/
+#  ------------------------------------------------------------------------------
+#
+# create enviroment vars to work with fastlane telegram
+echo TELEGRAM_WEBHOOKS=$TELEGRAM_WEBHOOKS > .env
+echo GIT_REPO=$CIRCLE_REPOSITORY_URL >> .env
+echo GIT_BRANCH=$CIRCLE_BRANCH >> .env
+
+# install Node.js v7
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo sudo apt-get install -y nodejs
+
+# install globally
+sudo npm install -g conventional-github-releaser
+
+# install node package available on package.json
+sudo npm install
+
+# config git
+git config --global user.email $GH_EMAIL
+git config --global user.name "Flyve MDM"
+git remote remove origin
+git remote add origin https://$GH_USER:$GH_TOKEN@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
+
+# Bintray information
+echo bintray.user=$BT_USER >> local.properties
+echo bintray.apikey=$BT_APIKEY >> local.properties
