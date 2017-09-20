5a48961788a4bb91cf59d5f872558ec4d48c6c74
Rafa Hernandez <rhernandez@teclib.com>
2017-09-20 17:43:28 -0400

    ci(release): comment push follow tag

--- ci/scripts/ci_github_release.sh
+++ ci/scripts/ci_github_release.sh
@@ -29,7 +29,7 @@
 GIT_TAG=$(jq -r ".version" package.json)
 
 # Push commits and tags to origin branch
-git push --follow-tags origin $CIRCLE_BRANCH
+# git push --follow-tags origin $CIRCLE_BRANCH
 
 # push tag to github
 conventional-github-releaser -t $GH_TOKEN
