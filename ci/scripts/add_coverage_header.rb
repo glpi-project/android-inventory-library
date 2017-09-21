08e3405f1a59c889f7c5c435a32ae051c47abd87
Rafa Hernandez <rhernandez@teclib.com>
2017-09-21 16:25:08 -0400

    ci(coverage): manage coverage report

--- ci/scripts/add_coverage_header.rb
+++ ci/scripts/add_coverage_header.rb
@@ -1,4 +1,3 @@
-#!/bin/bash
 #
 #  Copyright (C) 2017 Teclib'
 #
@@ -26,44 +25,18 @@
 #  ------------------------------------------------------------------------------
 #
 
-# create code coverage report
-./gradlew createDebugCoverageReport
+# Add header to all files on the folder reports/javadoc
+Dir.glob("reports/debug/**/*.html") do |search_file| # note one extra "*"
+    file = File.open("#{search_file}", "r+")
+    buffer = file.read
+    file.rewind
+    file.puts "---"
+    file.puts "layout: coverage"
+    file.puts "---"
+    file.print buffer
 
-# move code coverage
-mv -v inventory/build/reports/coverage reports$1
+    filtered_data = file.gsub("install", "latest")
+    file.write(filtered_data)
 
-#move Android test
-mv -v inventory/build/reports/androidTests reports$1
-
-# replace .resources with resource because github don't support folders with "_" or "." at the beginning
-mv reports$1/debug/.resources reports$1/debug/resources
-
-# add header
-ruby ci/scripts/add_coverage_header.rb
-
-# add code coverage and test result
-git add reports$1 -f
-
-# temporal commit
-git commit -m "tmp reports"
-
-# get gh-pages branch
-git fetch origin gh-pages
-
-# move to gh-pages
-git checkout gh-pages
-
-# clean
-sudo git clean -fdx
-
-# get documentation folder
-git checkout $CIRCLE_BRANCH reports$1
-
-# create commit
-git commit -m "docs(coverage): update code coverage and test result"
-
-# push to branch
-git push origin gh-pages
-
-# got back to original branch
-git checkout $CIRCLE_BRANCH
+    file.close
+end
