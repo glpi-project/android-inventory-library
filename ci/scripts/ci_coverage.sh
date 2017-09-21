5e027fe53ef0f6e66550310cb3ad2ff6594cc899
Rafa Hernandez <rhernandez@teclib.com>
2017-09-21 17:05:56 -0400

    ci(coverage): replace .sessions

--- ci/scripts/ci_coverage.sh
+++ ci/scripts/ci_coverage.sh
@@ -38,6 +38,9 @@ mv -v inventory/build/reports/androidTests reports$1
 # replace .resources with resource because github don't support folders with "_" or "." at the beginning
 mv reports$1/debug/.resources reports$1/debug/resources
 
+# replace .sessions
+mv reports$1/debug/.sessions.html reports$1/debug/sessions.html
+
 # add header
 ruby ci/scripts/add_coverage_header.rb
 
