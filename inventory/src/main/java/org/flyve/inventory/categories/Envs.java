305682072855e05c36bae803308f72e3041c0fcb
Rafa Hernandez <rhernandez@teclib.com>
2018-01-18 16:55:32 -0400

    fix(envs): manage exceptions

--- inventory/src/main/java/org/flyve/inventory/categories/Envs.java
+++ inventory/src/main/java/org/flyve/inventory/categories/Envs.java
@@ -34,6 +34,8 @@ package org.flyve.inventory.categories;
 
 import android.content.Context;
 
+import org.flyve.inventory.FILog;
+
 import java.util.Map;
 
 /**
@@ -60,13 +62,17 @@ public class Envs extends Categories {
 	public Envs(Context xCtx) {
         super(xCtx);
 
-        //get all envs vars
-        Map<String,String> envs = System.getenv();
-        for( Map.Entry<String,String> entry : envs.entrySet()) {
-           Category c = new Category("ENVS", "envs");
-           c.put("KEY", new CategoryValue(entry.getKey(),"KEY","key"));
-           c.put("VAL", new CategoryValue(envs.get(entry.getKey()),"VAL","Value"));
-           this.add(c);
+        try {
+            //get all envs vars
+            Map<String, String> envs = System.getenv();
+            for (Map.Entry<String, String> entry : envs.entrySet()) {
+                Category c = new Category("ENVS", "envs");
+                c.put("KEY", new CategoryValue(entry.getKey(), "KEY", "key"));
+                c.put("VAL", new CategoryValue(envs.get(entry.getKey()), "VAL", "Value"));
+                this.add(c);
+            }
+        } catch (Exception ex) {
+            FILog.e(ex.getMessage());
         }
     }
 }
