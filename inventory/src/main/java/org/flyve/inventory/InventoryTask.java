0bf5bfe2c70e89d6fe54bf688e9d076309b5fb7d
Rafa Hernandez <rhernandez@teclib.com>
2018-04-14 14:30:48 -0400

    feat(storage): add storage category
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- inventory/src/main/java/org/flyve/inventory/InventoryTask.java
+++ inventory/src/main/java/org/flyve/inventory/InventoryTask.java
@@ -117,6 +117,7 @@ public class InventoryTask {
 //                "PhoneStatus",
                 "Hardware",
                 "User",
+                "Storage",
                 "Bios",
                 "Memory",
                 "Inputs",
