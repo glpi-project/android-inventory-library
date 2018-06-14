16ccef44e8c654421b268a006e0d9ec175237234
Rafa Hernandez <rhernandez@teclib.com>
2018-06-14 18:58:54 -0400

    build(example): add new methods on the example
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- example_java/src/main/java/org/flyve/example_java/MainActivity.java
+++ example_java/src/main/java/org/flyve/example_java/MainActivity.java
@@ -3,14 +3,26 @@ package org.flyve.example_java;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import org.flyve.inventory.InventoryTask;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +51,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onTaskSuccess(String data) {
                         Log.d(TAG, data);
-                        inventoryTask.shareInventory( 2);
+                        //inventoryTask.shareInventory( 2);
+
+                        try {
+                            String base64 = data;
+                            getSyncWebData("http://10.0.0.6:8000/1e6dwka1", base64, null);
+                        } catch (Exception ex) {
+                            Log.e(TAG, ex.getMessage());
+                        }
                         Toast.makeText(MainActivity.this, "Inventory Success, check the log", Toast.LENGTH_SHORT).show();
                     }
 
@@ -66,6 +85,73 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public static String base64encode(String text) {
+        String rtext = "";
+        if(text == null) { return ""; }
+        try {
+            byte[] data = text.getBytes("UTF-8");
+            rtext = Base64.encodeToString(data, Base64.NO_WRAP | Base64.URL_SAFE);
+            rtext = rtext.replaceAll("-", "+");
+            rtext = rtext.replaceAll(" ", "+");
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        return rtext;
+    }
+
+    public static String getSyncWebData(final String url, final String data, final Map<String, String> header) {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            URL dataURL = new URL(url);
+            Log.d(TAG, "URL: " + url);
+            HttpURLConnection conn = (HttpURLConnection)dataURL.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setConnectTimeout(50000);
+            conn.setReadTimeout(500000);
+
+//            for (Map.Entry<String, String> entry : header.entrySet()) {
+//                conn.setRequestProperty(entry.getKey(), entry.getValue());
+//            }
+
+            // Send post request
+            conn.setDoOutput(true);
+
+            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+            os.writeBytes(data);
+            os.flush();
+            os.close();
+
+            if(conn.getResponseCode() >= 400) {
+                InputStream is = conn.getErrorStream();
+                return inputStreamToString(is);
+            }
+
+            InputStream is = conn.getInputStream();
+            return inputStreamToString(is);
+
+        }
+        catch (final Exception ex) {
+            String error = ex.getMessage();
+            Log.e(TAG, error);
+            return error;
+        }
+    }
+
+    private static String inputStreamToString(final InputStream stream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        br.close();
+        return sb.toString();
     }
 
 }
