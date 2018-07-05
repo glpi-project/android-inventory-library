38c9b18d35e09b84c3a890405b1453a495282125
Naylin <nmedina@teclib.com>
2018-07-05 16:34:43 -0400

    ci(circle): add new scripts
    Signed-off-by: Naylin <nmedina@teclib.com>

---
+++ ci/scripts/ci_generate_documentation.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+#  Copyright (C) 2017 Teclib'
+#
+#  This file is part of Flyve MDM Inventory Android.
+#
+#  Flyve MDM Inventory Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM Android is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM Inventory Android is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  ------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - rafaelje
+#  @copyright Copyright (c) 2017 Flyve MDM
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/flyve-mdm-android-inventory
+#  @link      http://www.glpi-project.org/
+#  @link      https://flyve-mdm.com/
+#  ------------------------------------------------------------------------------
+#
+
+# create code coverage report
+./gradlew :inventory:createDebugCoverageReport
+
+# move code coverage
+mv -v inventory/build/reports/coverage development
+
+# move Android test
+mv -v inventory/build/reports/androidTests development
+
+# rename folders to match respective section on project site
+mv development/debug development/coverage
+mv development/androidTests/connected development/test-reports
+
+# replace .resources with resource because github doesn't support folders with "_" or "." at the beginning
+mv development/coverage/.resources development/coverage/resources
+
+# find and replace links to the old name of file
+grep -rl .resources development/coverage/ | xargs sed -i 's|.resources|resources|g'
+
+# replace .sessions
+mv development/coverage/.sessions.html development/coverage/sessions.html
+
+# find and replace links to the old name of file
+grep -rl .sessions.html development/coverage/ | xargs sed -i 's|.sessions.html|sessions.html|g'
+
+# Generate javadoc this folder must be on .gitignore
+javadoc -d ./development/code-documentation -sourcepath ./inventory/src/main/java -subpackages . -nonavbar
+
+# delete the index.html file
+sudo rm ./development/code-documentation/index.html
+
+# rename the overview-summary.html file to index.html
+mv ./development/code-documentation/overview-summary.html ./development/code-documentation/index.html
+
+# find and replace links to the old name of file
+grep -rl overview-summary.html development/code-documentation/ | xargs sed -i 's|overview-summary.html|index.html|g'
+
+# send development folder to project site with the documentation updated, also removes the folder with old docs
+yarn gh-pages --dist ./development/ --dest ./development/ -m "docs(development): update documentation
+
+update coverage and test reports
+update code documentation"
