5d4cc6bb3882169c528536c874f70be2c643c4bf
Naylin Medina <nmedina@teclib.com>
2018-07-10 16:52:34 -0400

    docs(headers): update license
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_github_release.sh
+++ ci/scripts/ci_github_release.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 #
-#  Copyright (C) 2017 Teclib'
+#  LICENSE
 #
-#  This file is part of Flyve MDM Inventory Android.
+#  This file is part of Flyve MDM Inventory Library for Android.
 #
-#  Flyve MDM Inventory Android is a subproject of Flyve MDM. Flyve MDM is a mobile
-#  device management software.
+#  Inventory Library for Android is a subproject of Flyve MDM. Flyve MDM is a 
+#  mobile device management software.
 #
-#  Flyve MDM Android is free software: you can redistribute it and/or
+#  Flyve MDM is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
-#  Flyve MDM Inventory Android is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  Flyve MDM Inventory Library for Android is distributed in the hope that it 
+#  will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  ------------------------------------------------------------------------------
-#  @author    Rafael Hernandez - rafaelje
-#  @copyright Copyright (c) 2017 Flyve MDM
+#  --------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - <rhernandez@teclib.com>
+#  @author    Naylin Medina    - <nmedina@teclib.com>
+#  @copyright Copyright (c) Teclib'
 #  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
-#  @link      https://github.com/flyve-mdm/flyve-mdm-android-inventory
-#  @link      http://www.glpi-project.org/
+#  @link      https://github.com/flyve-mdm/android-inventory-library/
+#  @link      http://flyve.org/android-inventory-library/
 #  @link      https://flyve-mdm.com/
-#  ------------------------------------------------------------------------------
+#  --------------------------------------------------------------------------------
 #
 
 GIT_TAG=$(jq -r ".version" package.json)
