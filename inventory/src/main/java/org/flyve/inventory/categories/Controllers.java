8ba692707eaa9d474c816fc456d2892bdcb10bc6
Ivan Del Pino <idelpino@teclib.com>
2018-09-13 17:09:09 -0400

    feat(controllers): add information about controllers
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

---
+++ inventory/src/main/java/org/flyve/inventory/categories/Controllers.java
@@ -0,0 +1,124 @@
+/**
+ *
+ * Copyright 2017 Teclib.
+ * Copyright 2010-2016 by the FusionInventory Development
+ *
+ * http://www.fusioninventory.org/
+ * https://github.com/fusioninventory/fusioninventory-android
+ *
+ * ------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of FusionInventory project.
+ *
+ * FusionInventory is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * FusionInventory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @update    07/06/2017
+ * @license   GPLv2 https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @link      https://github.com/fusioninventory/fusioninventory-android
+ * @link      http://www.fusioninventory.org/
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.inventory.categories;
+
+import android.content.Context;
+
+import org.flyve.inventory.FILog;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * This class get all the information of the Controllers
+ */
+public class Controllers extends Categories {
+
+    /*
+     * The serialization runtime associates with each serializable class a version number,
+     * called a serialVersionUID, which is used during deserialization to verify that the sender
+     * and receiver of a serialized object have loaded classes for that object that are compatible
+     * with respect to serialization. If the receiver has loaded a class for the object that has a
+     * different serialVersionUID than that of the corresponding sender's class, then deserialization
+     * will result in an  InvalidClassException
+     *
+     *  from: https://stackoverflow.com/questions/285793/what-is-a-serialversionuid-and-why-should-i-use-it
+     */
+	private static final long serialVersionUID = 6791259866128400637L;
+
+    /**
+     * This constructor trigger get all the information about Controllers
+     * @param xCtx Context where this class work
+     */
+	public Controllers(Context xCtx) {
+        super(xCtx);
+        try {
+            ArrayList<String> drivers = getDrivers();
+            if (drivers.size() > 0) {
+                for (int i = 0; i < drivers.size(); i++) {
+                    Category c = new Category("CONTROLLERS", "controllers");
+                    c.put("DRIVER", new CategoryValue(drivers.get(i), "DRIVER", "driver"));
+                    this.add(c);
+                }
+            }
+        } catch (Exception ex) {
+            FILog.e(ex.getMessage());
+        }
+    }
+
+    /**
+     * Get the name Driver
+     * @return string name Driver
+     */
+    public ArrayList<String> getDrivers() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        File file = new File("/sys/bus/platform/drivers/");
+        File[] listFiles = file.listFiles();
+        if (listFiles != null) {
+            for (File file2 : listFiles) {
+                if (file2.isDirectory()) {
+                    String name = file2.getName();
+                    String a = filterEmptyFile(file2);
+                    if (a == null || a.isEmpty()) {
+                        arrayList2.add(name);
+                    } else {
+                        arrayList.add(name);
+                    }
+                }
+            }
+            if (arrayList.isEmpty()) {
+                arrayList.addAll(arrayList2);
+            }
+            Collections.sort(arrayList);
+        }
+        return arrayList;
+    }
+
+    private String filterEmptyFile(File file) {
+        File[] listFiles = file.listFiles();
+        if (listFiles == null) {
+            return null;
+        }
+        for (File file2 : listFiles) {
+            if (file2.isDirectory()) {
+                String name = file2.getName();
+                if (!("bind".equals(name) || "unbind".equals(name) || "uevent".equals(name))) {
+                    return name;
+                }
+            }
+        }
+        return null;
+    }
+
+}
