05234c92ad459704793753dbb1cbae72f8abf32f
Ivan Del Pino <idelpino@teclib.com>
2018-09-18 16:56:03 -0400

    feat(sim): add information about multiples Simcards
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- inventory/src/androidTest/java/org/flyve/inventory/SimcardsTest.java
+++ inventory/src/androidTest/java/org/flyve/inventory/SimcardsTest.java
@@ -1,29 +1,30 @@
 /**
- *  LICENSE
- * 
- *  This file is part of Flyve MDM Inventory Library for Android.
+ * LICENSE
+ * <p>
+ * This file is part of Flyve MDM Inventory Library for Android.
+ * <p>
+ * Inventory Library for Android is a subproject of Flyve MDM.
+ * Flyve MDM is a mobile device management software.
+ * <p>
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p>
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ---------------------------------------------------------------------
  *
- *  Inventory Library for Android is a subproject of Flyve MDM.
- *  Flyve MDM is a mobile device management software.
- * 
- *  Flyve MDM is free software: you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 3
- *  of the License, or (at your option) any later version.
- *
- *  Flyve MDM is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  ---------------------------------------------------------------------
- *  @author    Rafael Hernandez - <rhernandez@teclib.com>
- *  @copyright Copyright Teclib. All rights reserved.
- *  @copyright Copyright FusionInventory.
- *  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
- *  @link      https://github.com/flyve-mdm/android-inventory-library
- *  @link      http://flyve.org/android-inventory-library/
- *  @link      https://flyve-mdm.com
- *  ---------------------------------------------------------------------
+ * @author Rafael Hernandez - <rhernandez@teclib.com>
+ * @copyright Copyright Teclib. All rights reserved.
+ * @copyright Copyright FusionInventory.
+ * @license GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link https://github.com/flyve-mdm/android-inventory-library
+ * @link http://flyve.org/android-inventory-library/
+ * @link https://flyve-mdm.com
+ * ---------------------------------------------------------------------
  */
 
 package org.flyve.inventory;
@@ -31,11 +32,13 @@ package org.flyve.inventory;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.telephony.SubscriptionInfo;
 
 import org.flyve.inventory.categories.Simcards;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static org.junit.Assert.assertNotEquals;
 
@@ -47,43 +50,101 @@ public class SimcardsTest {
     @Test
     public void getCountry() throws Exception {
         Simcards simcards = new Simcards(appContext);
-        assertNotEquals("", simcards.getCountry());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            List<SubscriptionInfo> multipleSim = simcards.getMultipleSim(appContext);
+            if (multipleSim != null && multipleSim.size() > 0) {
+                for (SubscriptionInfo info : multipleSim)
+                    assertNotEquals("", info.getCountryIso());
+            }
+        } else {
+            assertNotEquals("", simcards.getCountry());
+        }
     }
 
     @Test
     public void getOperator_code() throws Exception {
         Simcards simcards = new Simcards(appContext);
-        assertNotEquals("", simcards.getOperatorCode());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            List<SubscriptionInfo> multipleSim = simcards.getMultipleSim(appContext);
+            if (multipleSim != null && multipleSim.size() > 0) {
+                for (SubscriptionInfo info : multipleSim) {
+                    String operatorCode = info.getMcc() + "" + info.getMnc();
+                    assertNotEquals("", operatorCode);
+                }
+            }
+        } else {
+            assertNotEquals("", simcards.getOperatorCode());
+        }
     }
 
     @Test
     public void getOperator_name() throws Exception {
         Simcards simcards = new Simcards(appContext);
-        assertNotEquals("", simcards.getOperatorName());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            List<SubscriptionInfo> multipleSim = simcards.getMultipleSim(appContext);
+            if (multipleSim != null && multipleSim.size() > 0) {
+                for (SubscriptionInfo info : multipleSim)
+                    assertNotEquals("", info.getCarrierName().toString());
+            }
+        } else {
+            assertNotEquals("", simcards.getOperatorName());
+        }
     }
 
     @Test
     public void getSerial() throws Exception {
         Simcards simcards = new Simcards(appContext);
-        assertNotEquals("", simcards.getSerial());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            List<SubscriptionInfo> multipleSim = simcards.getMultipleSim(appContext);
+            if (multipleSim != null && multipleSim.size() > 0) {
+                for (SubscriptionInfo info : multipleSim)
+                    assertNotEquals("", info.getIccId());
+            }
+        } else {
+            assertNotEquals("", simcards.getSerial());
+        }
     }
 
     @Test
     public void getState() throws Exception {
         Simcards simcards = new Simcards(appContext);
-        assertNotEquals("", simcards.getState());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            List<SubscriptionInfo> multipleSim = simcards.getMultipleSim(appContext);
+            if (multipleSim != null && multipleSim.size() > 0) {
+                for (SubscriptionInfo info : multipleSim)
+                    assertNotEquals("", simcards.getState(appContext, info.getSimSlotIndex()));
+            }
+        } else {
+            assertNotEquals("", simcards.getState(appContext, 0));
+        }
     }
 
     @Test
     public void getLine_number() throws Exception {
         Simcards simcards = new Simcards(appContext);
-        assertNotEquals("", simcards.getLineNumber());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            List<SubscriptionInfo> multipleSim = simcards.getMultipleSim(appContext);
+            if (multipleSim != null && multipleSim.size() > 0) {
+                for (SubscriptionInfo info : multipleSim)
+                    assertNotEquals("", info.getNumber());
+            }
+        } else {
+            assertNotEquals("", simcards.getLineNumber());
+        }
     }
 
     @Test
     public void getSubscriber_id() throws Exception {
         Simcards simcards = new Simcards(appContext);
-        assertNotEquals("", simcards.getSubscriberId());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            List<SubscriptionInfo> multipleSim = simcards.getMultipleSim(appContext);
+            if (multipleSim != null && multipleSim.size() > 0) {
+                for (SubscriptionInfo info : multipleSim)
+                    assertNotEquals("", String.valueOf(info.getSubscriptionId()));
+            }
+        } else {
+            assertNotEquals("", simcards.getSubscriberId());
+        }
     }
 
 }
