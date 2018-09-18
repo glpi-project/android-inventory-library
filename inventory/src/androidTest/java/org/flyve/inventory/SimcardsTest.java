825fe5073b9079b1437aecac088bfe120c6bb1b1
Ivan Del Pino <idelpino@teclib.com>
2018-09-18 17:41:15 -0400

    docs(header): remove tag
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- inventory/src/androidTest/java/org/flyve/inventory/SimcardsTest.java
+++ inventory/src/androidTest/java/org/flyve/inventory/SimcardsTest.java
@@ -1,22 +1,17 @@
 /**
  * LICENSE
- * <p>
  * This file is part of Flyve MDM Inventory Library for Android.
- * <p>
  * Inventory Library for Android is a subproject of Flyve MDM.
  * Flyve MDM is a mobile device management software.
- * <p>
  * Flyve MDM is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
- * <p>
+ * as published by the Free Software Foundation; either version 3
  * Flyve MDM is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * ---------------------------------------------------------------------
- *
  * @author Rafael Hernandez - <rhernandez@teclib.com>
  * @copyright Copyright Teclib. All rights reserved.
  * @copyright Copyright FusionInventory.
