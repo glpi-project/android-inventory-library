744624241c75f18da8da9ad72028dff883ac8245
Ivan Del Pino <idelpino@teclib.com>
2018-09-28 15:44:07 -0400

    feat(filog): handle exception error to the bios
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- inventory/src/main/java/org/flyve/inventory/CommonErrorType.java
+++ inventory/src/main/java/org/flyve/inventory/CommonErrorType.java
@@ -10,4 +10,16 @@ public class CommonErrorType {
     public static final int BATTERY_HEALTH = 105;
     public static final int BATTERY_STATUS = 106;
     public static final int BATTERY_CAPACITY = 107;
+
+    /* Bios */
+    public static final int BIOS_DATE = 201;
+    public static final int BIOS_MANUFACTURER = 202;
+    public static final int BIOS_VERSION = 203;
+    public static final int BIOS_BOARD_MANUFACTURER = 204;
+    public static final int BIOS_MOTHER_BOARD_MODEL = 205;
+    public static final int BIOS_TAG = 206;
+    public static final int BIOS_MOTHER_BOARD_SERIAL = 207;
+    public static final int BIOS_SYSTEM_SERIAL = 208;
+    public static final int BIOS_SERIAL_PRIVATE = 209;
+    public static final int BIOS_CPU_SERIAL = 210;
 }
