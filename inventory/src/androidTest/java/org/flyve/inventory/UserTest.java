5bf30d2062126598a8536175e6306a6b0ab69264
Ivan Del Pino <idelpino@teclib.com>
2018-10-01 11:13:38 -0400

    feat(filog): handle exception error to the User
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

---
+++ inventory/src/androidTest/java/org/flyve/inventory/UserTest.java
@@ -0,0 +1,53 @@
+/**
+ *  LICENSE
+ * 
+ *  This file is part of Flyve MDM Inventory Library for Android.
+ *
+ *  Inventory Library for Android is a subproject of Flyve MDM.
+ *  Flyve MDM is a mobile device management software.
+ * 
+ *  Flyve MDM is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  Flyve MDM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  ---------------------------------------------------------------------
+ *  @author    Rafael Hernandez - <rhernandez@teclib.com>
+ *  @author    Ivan del Pino    - <idelpino@teclib.com>
+ *  @copyright Copyright Teclib. All rights reserved.
+ *  @copyright Copyright FusionInventory.
+ *  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ *  @link      https://github.com/flyve-mdm/android-inventory-library
+ *  @link      http://flyve.org/android-inventory-library/
+ *  @link      https://flyve-mdm.com
+ *  ---------------------------------------------------------------------
+ */
+
+package org.flyve.inventory;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.flyve.inventory.categories.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class UserTest {
+
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    @Test
+    public void getClassTest() {
+        User user = new User(appContext);
+        assertNotEquals("", user.getUserName());
+    }
+
+}
--- inventory/src/main/java/org/flyve/inventory/CommonErrorType.java
+++ inventory/src/main/java/org/flyve/inventory/CommonErrorType.java
@@ -1,3 +1,32 @@
+/**
+ *  LICENSE
+ *
+ *  This file is part of Flyve MDM Inventory Library for Android.
+ *
+ *  Inventory Library for Android is a subproject of Flyve MDM.
+ *  Flyve MDM is a mobile device management software.
+ *
+ *  Flyve MDM is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  Flyve MDM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  ---------------------------------------------------------------------
+ *  @author    Rafael Hernandez - <rhernandez@teclib.com>
+ *  @author    Ivan del Pino    - <idelpino@teclib.com>
+ *  @copyright Copyright Teclib. All rights reserved.
+ *  @copyright Copyright FusionInventory.
+ *  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ *  @link      https://github.com/flyve-mdm/android-inventory-library
+ *  @link      http://flyve.org/android-inventory-library/
+ *  @link      https://flyve-mdm.com
+ *  ---------------------------------------------------------------------
+ */
+
 package org.flyve.inventory;
 
 public class CommonErrorType {
@@ -167,4 +196,8 @@ public class CommonErrorType {
     public static final int USB_REPORTED_PRODUCT_NAME = 1906;
     public static final int USB_USB_VERSION = 1907;
     public static final int USB_SERIAL_NUMBER = 1908;
+
+    /* User */
+    public static final int USER = 2000;
+    public static final int USER_NAME = 2001;
 }
