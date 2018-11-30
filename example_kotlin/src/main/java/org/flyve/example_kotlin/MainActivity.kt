427f9147a033e5445cca65d8f3b405c557d80593
Ivan Del Pino <idelpino@teclib.com>
2018-11-30 13:49:31 -0400

    feat: changes in the views example
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- example_kotlin/src/main/java/org/flyve/example_kotlin/MainActivity.kt
+++ example_kotlin/src/main/java/org/flyve/example_kotlin/MainActivity.kt
@@ -1,23 +1,43 @@
 package org.flyve.example_kotlin
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import org.flyve.inventory.InventoryTask
 
 class MainActivity : AppCompatActivity() {
 
     val LOG = "inventory.example"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        callTestActivity.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
+        permission()
+        textHello.setOnClickListener({ generateTask() })
+    }
 
+    private fun permission() {
+        ActivityCompat.requestPermissions(this@MainActivity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA), 1)
+    }
+
+    private fun generateTask() {
         InventoryTask.showFILog = true
-//        val categories: Array<String> = arrayOf("Hardware", "OperatingSystem")
-        val inventoryTask = InventoryTask(this@MainActivity, "example-app-kotlin", false)
+        //        val categories: Array<String> = arrayOf("Hardware", "OperatingSystem")
+        val appVersion = "example-app-kotlin"
+        val inventoryTask = InventoryTask(this@MainActivity, appVersion, false)
         inventoryTask.tag = "Android Lenovo"
-        inventoryTask.getXML( object : InventoryTask.OnTaskCompleted {
+        inventoryTask.getXML(object : InventoryTask.OnTaskCompleted {
             override fun onTaskSuccess(data: String) {
                 Log.d(LOG, data)
             }
@@ -25,8 +45,8 @@ class MainActivity : AppCompatActivity() {
             override fun onTaskError(error: Throwable) {
                 Log.e(LOG, error.message)
             }
-        } )
-        inventoryTask.getJSON( object : InventoryTask.OnTaskCompleted {
+        })
+        inventoryTask.getJSON(object : InventoryTask.OnTaskCompleted {
             override fun onTaskSuccess(data: String?) {
                 Log.d(LOG, data)
             }
@@ -36,6 +56,5 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-
     }
 }
