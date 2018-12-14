3fe9d4fbd27d54df38cc3fb46560bd42801813d5
Naylin Medina <nmedina@teclib.com>
2018-12-14 16:48:20 -0400

    ci(documentation): remove coverage and test report
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_documentation.sh
+++ ci/scripts/ci_documentation.sh
@@ -33,7 +33,7 @@ if [[ $GH_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* &
 
 # run generatedocumentation script
 ci/scripts/ci_generate_documentation.sh
-ci/scripts/ci_coverage.sh
+# ci/scripts/ci_coverage.sh
 
 # Update layouts and styles of development folder for correct display on project site
 
@@ -48,15 +48,15 @@ sudo git clean -fdx
 
 # remove default stylesheet.css
 sudo rm ./development/code-documentation/$CIRCLE_BRANCH/stylesheet.css
-sudo rm ./development/coverage/$CIRCLE_BRANCH/resources/report.css
-sudo rm ./development/test-report/$CIRCLE_BRANCH/css/base-style.css
-sudo rm ./development/test-report/$CIRCLE_BRANCH/css/style.css
+# sudo rm ./development/coverage/$CIRCLE_BRANCH/resources/report.css
+# sudo rm ./development/test-report/$CIRCLE_BRANCH/css/base-style.css
+# sudo rm ./development/test-report/$CIRCLE_BRANCH/css/style.css
 
 # add new css
 cp ./css/codeDocumentation.css ./development/code-documentation/$CIRCLE_BRANCH/stylesheet.css
-cp ./css/coverage.css ./development/coverage/$CIRCLE_BRANCH/resources/report.css
-cp ./css/testReports.css ./development/test-report/$CIRCLE_BRANCH/css/style.css
-touch ./development/test-report/$CIRCLE_BRANCH/css/base-style.css
+# cp ./css/coverage.css ./development/coverage/$CIRCLE_BRANCH/resources/report.css
+# cp ./css/testReports.css ./development/test-report/$CIRCLE_BRANCH/css/style.css
+# touch ./development/test-report/$CIRCLE_BRANCH/css/base-style.css
 
 # change headers
 ruby ci/add_header.rb
