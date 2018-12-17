9ee1b8ac2df22ed357a01234caf0ce8e5b3fc8e5
Ivan Del Pino <idelpino@teclib.com>
2018-12-17 11:28:25 -0400

    ci(enviroment): change names enviroments
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- ci/scripts/ci_setup.sh
+++ ci/scripts/ci_setup.sh
@@ -50,7 +50,7 @@ sudo sudo apt-get install -y nodejs
 yarn install
 
 # config git
-git config --global user.email $GITHUB_EMAIL
+git config --global user.email $GH_EMAIL
 git config --global user.name "Teclib"
 git remote remove origin
-git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
+git remote add origin https://$GH_USER:$GH_TOKEN@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
