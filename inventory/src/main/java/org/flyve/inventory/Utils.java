deb61dc26e08c9bafa670ffcf33a71324aee9484
Teclib <skita@teclib.com>
2019-12-18 19:05:58 +0100

    fix(network): detect correctly network description
    Signed-off-by: Teclib <skita@teclib.com>

--- inventory/src/main/java/org/flyve/inventory/Utils.java
+++ inventory/src/main/java/org/flyve/inventory/Utils.java
@@ -49,12 +49,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
@@ -352,7 +356,7 @@ public class Utils {
         try {
             Scanner s = new Scanner(new File(path));
             return s.next();
-        } catch (Exception ex) {
+        } catch (FileNotFoundException ex) {
             InventoryLog.e(InventoryLog.getMessage(String.valueOf(CommonErrorType.UTILS_CAT_INFO), ex.getMessage()));
         }
         return "";
@@ -518,6 +522,33 @@ public class Utils {
         } else {
             InventoryLog.d("External Storage is not available");
         }
+    }
+
+    public static String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ignored) { } // for now eat exceptions
+        return "";
     }
 
 }
